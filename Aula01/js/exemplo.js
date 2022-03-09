574567c44ed08448442457c58216b73aa22f337a
Renan Costa Filgueiras <renan2727@hotmail.com>
2022-03-09 10:32:49 -0300

    Aula2 - Soma

--- Aula01/js/exemplo.js
+++ Aula01/js/exemplo.js
@@ -1,4 +1,14 @@
-var valor;
-valor = document.getElementById("v1");
-alert(parseInt(10.15,10)+"ABC");
-alert(valor);
+alert("Bem-vindo!");
+
+function exibeValor(){
+    var v1,v2,soma;
+    v1 = document.getElementById("v1").value;
+    v2 = document.getElementById("v2").value;
+    soma = parseInt(v1,10)+parseInt(v2,10);
+    if(!isNaN(soma)){
+        alert(soma);
+    } else {
+        alert("Informe um número"); 
+    }
+    
+}
